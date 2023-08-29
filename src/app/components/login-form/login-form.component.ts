1d7ac79e72be0939f1e4090c743fb8f4fb80026d
Max-munene <munenemaxwell2@gmail.com>
2023-08-29 11:48:14 +0300

    form-validation-modelClass

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -3,10 +3,16 @@ import { MatFormField } from '@angular/material/form-field';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { FormControl } from '@angular/forms';
+import { LoginModel } from 'src/app/login-model';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
+  // imports:[FormControl]
   // imports:[MatFormFieldModule,MatIconModule,MatInputModule],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  hide: boolean = false;
+  userLogin = new LoginModel('', '', '', '');
+}
---
+++ src/app/login-model.ts
@@ -0,0 +1,8 @@
+export class LoginModel {
+  constructor(
+    public name: string,
+    public email: string,
+    public password: string,
+    public select: string
+  ) {}
+}
