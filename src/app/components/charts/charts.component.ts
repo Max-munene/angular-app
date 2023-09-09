6d97cefdf0f9407b919c18c2e5b4d823f7e951c8
Max-munene <munenemaxwell2@gmail.com>
2023-09-09 22:49:06 +0300

    more-graph and color

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -32,18 +32,18 @@ export class ChartsComponent implements OnInit {
               label: 'My test Data Graph',
               data: [
                 '10',
-                '20',
+                '250',
                 '35',
-                '40',
+                '80',
                 '56',
-                '10',
-                '25',
-                '36',
-                '46',
-                '100',
+                '5',
+                '28',
+                '3',
+                '49',
+                '110',
               ],
               backgroundColor: ['#fff'],
-              borderColor: ['#3f51b5'],
+              borderColor: ['#DFBE7D'],
               tension: 0.35,
             },
           ],
@@ -62,7 +62,70 @@ export class ChartsComponent implements OnInit {
           },
           animations: {
             tension: {
-              duration: 1000,
+              duration: 500,
+              easing: 'linear',
+              from: 0.5,
+              to: 0,
+              loop: true,
+            },
+          },
+        },
+      }
+    );
+
+    this.chart = new Chart(
+      document.getElementById('graph2') as HTMLCanvasElement,
+      {
+        type: 'line',
+        data: {
+          labels: [
+            '2000',
+            '2001',
+            '2002',
+            '2003',
+            '2004',
+            '2005',
+            '2006',
+            '2007',
+            '2008',
+            '2009',
+          ],
+          datasets: [
+            {
+              label: 'My test Data Graph',
+              data: [
+                '10',
+                '60',
+                '35',
+                '40',
+                '100',
+                '10',
+                '25',
+                '36',
+                '80',
+                '100',
+              ],
+              backgroundColor: ['#fff'],
+              borderColor: ['#144B73'],
+              tension: 0.35,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            x: {
+              display: true,
+            },
+            y: {
+              display: true,
+              beginAtZero: true,
+            },
+          },
+          animations: {
+            tension: {
+              duration: 2000,
               easing: 'linear',
               from: 0.5,
               to: 0,
